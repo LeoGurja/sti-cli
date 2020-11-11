48ada90a647e30ab2b1ed93324e584959388e33d
Leonardo Gurgel <lgurgel@id.uff.br>
2020-11-11 15:48:39 -0300

    add update-notifier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 
-import packageJson from '../package.json'
+import updateNotifier from 'update-notifier'
+import pkg from '../package.json'
 import Base from './base'
 import subCommands from './subcommands'
 require('@babel/register')({ extensions: ['.js', '.ts'] })
 
-const cli = new Base('sti', packageJson.version)
+const cli = new Base('sti', pkg.version)
 cli.useSubCommands(subCommands)
+
+updateNotifier({ pkg }).notify()
 
 cli.run(process.argv)
