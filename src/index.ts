650375e16238779d9101e55a37381206cc1adc0b
Leonardo Gurgel <lgurgel@id.uff.br>
2020-11-11 15:20:07 -0300

    add jest

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import packageJson from '../package.json'
 import Base from './base'
 import subCommands from './subcommands'
+require('@babel/register')({ extensions: ['.js', '.ts'] })
 
 const cli = new Base('sti', packageJson.version)
 cli.useSubCommands(subCommands)
