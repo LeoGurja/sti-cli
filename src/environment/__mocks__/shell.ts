7a5dc66762873ec28421530d8f9c7f3084e6974c
Leonardo Gurgel <lgurgel@id.uff.br>
2021-06-17 23:34:22 -0300

    fix mock

--- src/environment/__mocks__/shell.ts
+++ src/environment/__mocks__/shell.ts
@@ -1,6 +1,4 @@
-export default {
-  exec: jest.fn(() => ({ code: 0 })),
-  rm: jest.fn(() => ({ code: 0 })),
-  ls: jest.fn(() => ({ code: 1 })),
-  mkdir: jest.fn(() => ({ code: 0 }))
-}
+export const exec = jest.fn(() => ({ code: 0 }))
+export const rm = jest.fn(() => ({ code: 0 }))
+export const ls = jest.fn(() => ({ code: 1 }))
+export const mkdir = jest.fn(() => ({ code: 0 }))
